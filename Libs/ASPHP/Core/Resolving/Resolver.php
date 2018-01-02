a1b30324034f20782837c522fb0f386d4f56f7a8
Vigan Abdurrahmani <vigan.abd@outlook.com>
2018-01-02 01:36:36 +0100

    Resolver Linux Fix

--- Libs/ASPHP/Core/Resolving/Resolver.php
+++ Libs/ASPHP/Core/Resolving/Resolver.php
@@ -20,49 +20,41 @@ class Resolver extends \ASPHP\Core\Types\StaticClass
     public static function Resolve($classname)
     {
 		$root = Config::Get()["environment"]["directory"]["~"];
-		if(file_exists($root.'/'.trim($classname, '/\\').'.php'))
+		if(file_exists(static::path_reslover($root.'/'.trim($classname, '/\\').'.php')))
 		{
-			require_once $root.'/'.trim($classname, '/\\').'.php';
+			require_once static::path_reslover($root.'/'.trim($classname, '/\\').'.php');
 			return $classname;
 		}
-		else if(file_exists($root.'/Libs/'.trim($classname, '/').'.php'))
+		else if(file_exists(static::path_reslover($root.'/Libs/'.trim($classname, '/').'.php')))
 		{
-			require_once $root.'/Libs/'.trim($classname, '/\\').'.php';
+			require_once static::path_reslover($root.'/Libs/'.trim($classname, '/\\').'.php');
 			return $classname;
 		}
-		else if(!empty($GLOBALS['aliases'][$classname]) && file_exists($root.'/'.trim($GLOBALS['aliases'][$classname], '/\\').'.php'))
+		else if(!empty($GLOBALS['aliases'][$classname]) && file_exists(static::path_reslover($root.'/'.trim($GLOBALS['aliases'][$classname], '/\\').'.php')))
 		{
-			require_once $root.'/'.trim($GLOBALS['aliases'][$classname], '/\\').'.php';
+			require_once static::path_reslover($root.'/'.trim($GLOBALS['aliases'][$classname], '/\\').'.php');
 			class_alias($GLOBALS['aliases'][$classname], $classname);
 			return $GLOBALS['aliases'][$classname];
 		}
-		else if(!empty($GLOBALS['aliases'][$classname]) && file_exists($root.'/Libs/'.trim($GLOBALS['aliases'][$classname], '/\\').'.php'))
+		else if(!empty($GLOBALS['aliases'][$classname]) && file_exists(static::path_reslover($root.'/Libs/'.trim($GLOBALS['aliases'][$classname], '/\\').'.php')))
 		{
-			require_once $root.'/Libs/'.trim($GLOBALS['aliases'][$classname], '/\\').'.php';
+			require_once static::path_reslover($root.'/Libs/'.trim($GLOBALS['aliases'][$classname], '/\\').'.php');
 			class_alias($GLOBALS['aliases'][$classname], $classname);
 			return $GLOBALS['aliases'][$classname];
 		}
 		return "";
-		//if(!isset(static::$config))
-		//    static::$config = json_decode(file_get_contents(Config::Get()["environment"]["directory"]["~"].'/App_Start/resolver.config.json'), true);
+	}
 
-		//if(key_exists($classname, static::$config['aliases']))
-		//{
-		//    require_once Config::Get()["environment"]["directory"]["~"].static::$config['paths'][static::$config['aliases'][$classname]];
-		//    return static::$config['aliases'][$classname];
-		//}
-		//else
-		//{
-		//    foreach (static::$config['aliases'] as $k => $v)
-		//    {
-		//        if(preg_match("/".$k."/", $classname))
-		//        {
-		//            require_once Config::Get()["environment"]["directory"]["~"].static::$config['paths'][$v];
-		//            return $v;
-		//        }
-		//    }
-		//}
-		//return "";
-    }
+	public static function path_reslover($path)
+	{
+		if(PHP_OS == 'WINNT')
+		{
+			return str_replace("/", "\\", $path);
+		}
+		else
+		{
+			return str_replace("\\", "/", $path);
+		}
+	}
 }
 ?>
